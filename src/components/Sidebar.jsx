21c8f46fda62ef4084db6fc9214643a159344c21
Anshika31sharma <sharma.anshika7251@gmail.com>
2025-06-20 01:25:21 +0530

    fix: update title in index.html to 'Admin Panel'; adjust height in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import edit from '../../src/Assets/EditBlue.svg';
 
 export default function Sidebar() {
   return (
-  <aside className="w-48 sm:w-56 md:w-60 lg:w-64 mt-10  fixed top-0 left-0 z-10 shadow-md bg-white h-[140dvh] min-h-screen">
+  <aside className="w-48 sm:w-56 md:w-60 lg:w-64 mt-10  fixed top-0 left-0 z-10 shadow-md bg-white h-[150dvh] min-h-screen">
   <nav className="mt-4">
     <ul>
       <li className="flex items-center gap-2 sm:gap-3 px-4 sm:px-6 py-2 sm:py-3">
